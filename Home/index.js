361ac13264820bb9be79f1dc0f2b11fc5d8537ff
Majorbangz88 <joelchimaobichukwu@gmail.com>
2024-09-04 00:00:52 +0100

    added second section after nav bar

--- Home/index.js
+++ Home/index.js
@@ -4,6 +4,7 @@ const playPauseDiv = document.getElementById('play-pause-btn');
 function togglePlay() {
     if (video.paused) {
         video.play();
+        video.loop = true;
         playPauseDiv.innerHTML = '<i class="fas fa-pause"></i>';
     } else {
         video.pause();
